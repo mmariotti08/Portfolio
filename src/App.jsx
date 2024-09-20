06c81f06a8e8d32bf8c56404c4497bcc16adeee4
mmariotti08 <mariottimatias08@gmail.com>
2024-09-20 09:52:47 -0300

    initialCommit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>volviste wacho</div>
+  )
+}
+
+export default NavBar
